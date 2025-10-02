929951c9520aa32f895dfd83e3cbf09db0d735a4
Gabriel Thornes <gthornes@tuebingen.mpg.de>
2025-10-02 10:19:52 +0200

    create conda env

---
+++ src/conda_env.sh
@@ -0,0 +1,4 @@
+micromamba install -p /tmp/global2/gthornes/miniforge3/envs/dronetanalysis/ r-base python -y
+micromamba install r-data.table r-ggplot2 r-dplyr r-tidyr r-readr r-purrr r-stringr \
+                    r-reshape2 r-glue \ 
+                     -p /tmp/global2/gthornes/miniforge3/envs/dronetanalysis/ -y
