3116d4ff73d623121065287acbd0e8b894bd1f8d
utcao <yutao.cao@tuebingen.mpg.de>
2025-10-02 11:50:01 +0200

    create a template

---
+++ src/scripts/create_net/01create_net.R
@@ -0,0 +1,6 @@
+# To-do list
+# 1. read confg file & parameters
+# 2. define creeate_network()
+# 3. apply the function to the input
+
+# Rscript src/scripts/create_net/01create_net.R -files file_A.csv -output network_A.csv
