d84f230d8cf108517327eef26ee3fb65d9c3e290
Gabriel Thornes <g.thornes@outlook.com>
2025-10-02 16:30:06 +0200

    updated conda_env

--- src/conda_env.sh
+++ src/conda_env.sh
@@ -2,3 +2,5 @@ micromamba install -p /tmp/global2/gthornes/miniforge3/envs/dronetanalysis/ r-ba
 micromamba install r-data.table r-ggplot2 r-dplyr r-tidyr r-readr r-purrr r-stringr \
                     r-reshape2 r-glue \ 
                      -p /tmp/global2/gthornes/miniforge3/envs/dronetanalysis/ -y
+micromamba install r-data.table r-ggplot2 r-dplyr r-tidyr r-readr r-purrr r-stringr \
+                    r-reshape2 r-glue -y
