7b7a0462ebfb3b9d74d43de91036fdfafeb02b52
Gabriel Thornes <g.thornes@outlook.com>
2025-10-14 17:07:34 +0200

    permutation matrix converter draft code

---
+++ src/scripts/perm_matrix_convert/perm_matrix_convert.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env Rscript
+# ==============================================================================
+# Convert long p values of gene pairs matrix to wide matrix
+#
+# Script by Gabriel Thornes
+#
+# Last Updated: 14/10/2025
+#
+# This script::
+#   1. Takes p values of gene pairs matrix datasets as input
+#   2. Outputs p x p matrix
+# ==============================================================================
+
