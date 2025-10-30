7994f7ff2bc85ab8ce0c710feca9d1f439073450
Gabriel Thornes <g.thornes@outlook.com>
2025-10-30 15:44:54 +0100

    removal of signed adjaceny calculations

--- src/scripts/soft_threshold/adjacency_matrix.R
+++ src/scripts/soft_threshold/adjacency_matrix.R
@@ -4,13 +4,14 @@
 #
 # Script by Gabriel Thornes
 #
-# Last Updated: 27/10/2025
+# Last Updated: 30/10/2025
 #
 # This script::
 #   1. Takes spearman correlation matrix as input
-#   2. Calculates adjacency matrices
-#   3. Generates plots to decide soft-thresholding power
-#   4. Outputs adjacency matrices, plots and fit indices as CSV files in appropriate folder
+#   2. Creates binary matrix to preserve negative correlations
+#   3. Calculates adjacency matrices
+#   4. Generates plots to decide soft-thresholding power
+#   5. Outputs adjacency matrices, plots and fit indices as CSV files in appropriate folder
 # ==============================================================================
 
 rm(list = ls())
@@ -27,9 +28,9 @@ source("src/utils/utils_io.R")
 source("src/utils/utils_network_feats.R")
 config <- yaml::read_yaml("config/config.yaml")
 matrix_dir <- config$output_dirs$spearman_dir
-matrix_file <- file.path(matrix_dir, "spearman_correlation_matrix.csv")
+matrix_file <- file.path(matrix_dir, "spearman_matrices/spearman_correlation_matrix.csv")
 output_dir <- config$output_dirs$network_features_dir
-signed_output_file <- file.path(output_dir, "soft_threshold/signed/signed_adjacency_matrix.csv")
+signed_output_file <- file.path(output_dir, "binary_signed_matrix.csv")
 unsigned_output_file <- file.path(output_dir, "soft_threshold/unsigned/unsigned_adjacency_matrix.csv")
 
 corr_matrix <- fread(matrix_file)
@@ -37,48 +38,23 @@ cat("File read:", matrix_file,"\n")
 df.corr.m <- as.matrix(corr_matrix[,-1, with=FALSE])
     rownames(df.corr.m) <- corr_matrix[[1]]
 
-# ----- 3. Calculate adjacency matrices and generate soft thresholding plots -----
+# ----- 3. Create binary matrix to preserve negative correlations -----
+binary_matrix <- create_correlation_sign_matrix(df.corr.m, output_file = signed_output_file)
+cat("Binary sign matrix created.\n")
 
-# Option 1: Signed network (preserves directionality)
-# Convert correlations to signed adjacency (keeps positive/negative relationships)
-# Values range from -1 to +1, then transformed to 0 to 1 as: (1 + cor) / 2
-signed_adjacency <- (1 + df.corr.m) / 2
-
-# Option 2: Traditional unsigned network (magnitude only)
+# ----- 4. Calculate adjacency matrices and generate soft thresholding plots -----
 unsigned_adjacency <- abs(df.corr.m) # adjacency matrix
 
 # Save the adjacency matrices
-write.csv(signed_adjacency, signed_output_file, row.names = TRUE)
 write.csv(unsigned_adjacency, unsigned_output_file, row.names = TRUE)
-cat("Signed adjacency matrix saved to:", signed_output_file, "\n")
 cat("Unsigned adjacency matrix saved to:", unsigned_output_file, "\n")
 
-# -----4. Generate soft thresholding plots to decide on appropriate power -----
+# ----- 5. Generate soft thresholding plots to decide on appropriate power -----
 
 # Generate soft-thresholded matrices and plots
 
-##############################
-## Option 1: Signed network ##
-##############################
-
-# Call the network topology analysis function for signed network
-sft_signed <- pickSoftThreshold(signed_adjacency, powerVector = c(1:30), 
-                                networkType = "signed", verbose = 5)
-
-# Generate plots to decide soft-thresholding power
-s_output_plot <- file.path(output_dir, "soft_threshold/signed/soft_thresholding_signed.pdf")
-sft_plot(sft_signed, s_output_plot, c(1:30))
-
-# Write sft to file for analysis
-signed_output_file <- file.path(output_dir, "soft_threshold/signed/signed_soft_threshold.csv")
-write.csv(sft_signed$fitIndices, file = signed_output_file, row.names = FALSE)
-
-################################
-## Option 2: Unsigned network ##
-################################
-
 # Call the network topology analysis function for unsigned network
-sft_unsigned <- pickSoftThreshold(unsigned_adjacency, powerVector = c(1:20), # lower power range for unsigned
+sft_unsigned <- pickSoftThreshold(unsigned_adjacency, powerVector = c(1:20),
                                   networkType = "unsigned", verbose = 5)
 
 # Generate plots to decide soft-thresholding power
--- src/scripts/soft_threshold/apply_threshold.R
+++ src/scripts/soft_threshold/apply_threshold.R
@@ -4,7 +4,7 @@
 #
 # Script by Gabriel Thornes
 #
-# Last Updated: 27/10/2025
+# Last Updated: 30/10/2025
 #
 # This script::
 #   1. Takes adjacency matrices as input
@@ -26,37 +26,20 @@ source("src/utils/utils_io.R")
 source("src/utils/utils_network_feats.R")
 config <- yaml::read_yaml("config/config.yaml")
 soft_threshold_dir <- config$output_dirs$network_features_dir
-s_soft_threshold_file <- file.path(soft_threshold_dir, "soft_threshold/signed/signed_adjacency_matrix.csv")
-u_soft_threshold_file <- file.path(soft_threshold_dir, "soft_threshold/unsigned/unsigned_adjacency_matrix.csv")
+soft_threshold_file <- file.path(soft_threshold_dir, "soft_threshold/unsigned/unsigned_adjacency_matrix.csv")
 output_dir <- config$output_dirs$network_features_dir
 
 # ----- 3. Load adjacency matrices -----
-sft_signed <- read.csv(s_soft_threshold_file, row.names = 1)
-cat("File read:", s_soft_threshold_file,"\n")
 
-sft_unsigned <- read.csv(u_soft_threshold_file, row.names = 1)
-cat("File read:", u_soft_threshold_file,"\n")
+sft_unsigned <- read.csv(soft_threshold_file, row.names = 1)
+cat("File read:", soft_threshold_file,"\n")
 
 # ----- 4. Apply soft-thresholding -----
 
 ## Apply soft-thresholding power based on plot analysis ##
 
-####################
-## Signed network ##
-####################
-
-soft_power_signed <- 10  ### Example power, adjust based on plot analysis ###
-soft_signed_adj <- sft_signed^soft_power_signed
-signed_output_file <- file.path(output_dir, "soft_threshold/signed/signed_soft_thresholded_adjacency_matrix.csv")
-write.csv(soft_signed_adj, signed_output_file, row.names = TRUE)
-cat("Signed soft-thresholded adjacency matrix saved to:", signed_output_file, "\n")
-
-######################
-## Unsigned network ##
-######################
-
-soft_power_unsigned <- 3  ### Example power, adjust based on plot analysis ###
-soft_unsigned_adj <- sft_unsigned^soft_power_unsigned
-unsigned_output_file <- file.path(output_dir, "soft_threshold/unsigned/unsigned_soft_thresholded_adjacency_matrix.csv")
-write.csv(soft_unsigned_adj, unsigned_output_file, row.names = TRUE)
-cat("Unsigned soft-thresholded adjacency matrix saved to:", unsigned_output_file, "\n")
+soft_power <- 4  ### Example power, adjust based on plot analysis ###
+soft_adj <- sft_unsigned^soft_power
+output_file <- file.path(output_dir, "soft_threshold/unsigned/unsigned_soft_thresholded_adjacency_matrix.csv")
+write.csv(soft_adj, output_file, row.names = TRUE)
+cat("Unsigned soft-thresholded adjacency matrix saved to:", output_file, "\n")
