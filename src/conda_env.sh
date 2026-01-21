4c7e0736cb87e06ae1eca7a86feaf438f077b384
utcao <yutao.cao@tuebingen.mpg.de>
2026-01-21 13:39:00 +0100

    make consistence name for input files

--- src/conda_env.sh
+++ src/conda_env.sh
@@ -1,10 +1,14 @@
-conda install -p /tmp/global2/gthornes/miniforge3/envs/dronetanalysis/ r-base python -y
+# conda env packages
+# conda activate dronetanalysis
+conda_env_path=/tmp/global2/caoyt/miniforge3/envs/dronetanalysis
+conda create -p $conda_env_path r-base=4.3 python=3.12 -y
 
 # Install R packages
-conda install -p /tmp/global2/gthornes/miniforge3/envs/dronetanalysis/ \
-    r-data.table r-ggplot2 r-dplyr r-tidyr r-readr r-purrr r-stringr \
-    r-reshape2 r-glue r-yaml r-limma r-edgeR -y
+micromamba install -p $conda_env_path \
+    r-data.table r-reshape2 r-ggplot2 r-dplyr r-tidyr r-readr r-purrr r-stringr \
+    r-dofuture r-foreach \
+    r-futile.logger r-glue r-yaml r-argparse r-igraph bioconda::bioconductor-limma bioconda::bioconductor-edger bioconda::r-wgcna -y
 
 # Install Python packages
-conda install -p /tmp/global2/gthornes/miniforge3/envs/dronetanalysis/ \
+micromamba install -p $conda_env_path \
     numpy pandas scikit-learn matplotlib jupyter biopython -y
