53ba65751d301920412cb9c462d5276da885fbf9
utcao <yutao.cao@tuebingen.mpg.de>
2026-04-14 12:04:21 +0200

    add summarize-mad-all-genes command

--- tmp/run_summarize_genes_mad_variability.sh
+++ tmp/run_summarize_genes_mad_variability.sh
@@ -1,1 +1,4 @@
+#ct 2026 04
 Rscript code/dronetanalysis/src/scripts/15analysis/summarize_all_genes_mad_variability.R --expr-file data/processed/VOOM/voomdataCtrl.txt --mapping-file run_voomct/results_ct_voom/rewiring_hubs_ct_anno_0408_2026.tsv --output-file results/variability/voomct_all_genes_mad_summary.xlsx --condition-label "Control"
+# hs 2026 0413
+#Rscript code/dronetanalysis/src/scripts/15analysis/summarize_all_genes_mad_variability.R --expr-file data/processed/VOOM/voomdataHS.txt --mapping-file run_voomhs/results_hs_voom/rewiring_hubs_hs_anno_0413_2026.tsv --output-file results/variability/voomhs_all_genes_mad_summary.xlsx --condition-label "HS"
