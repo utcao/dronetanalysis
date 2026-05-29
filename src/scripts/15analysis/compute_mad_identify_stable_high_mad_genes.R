553ba1881f7c5b64860290733711b7c227aaf1e4
utcao <yutao.cao@tuebingen.mpg.de>
2026-05-29 19:38:21 +0200

    fix bug in log2 by adding a pseudo count 1

--- src/scripts/15analysis/compute_mad_identify_stable_high_mad_genes.R
+++ src/scripts/15analysis/compute_mad_identify_stable_high_mad_genes.R
@@ -49,9 +49,9 @@ suppressPackageStartupMessages({
 
 # ----- Row-wise MAD: use matrixStats if available, else apply fallback -----
 row_mads <- if (requireNamespace("matrixStats", quietly = TRUE)) {
-  function(mat) log2(matrixStats::rowMads(2^mat, na.rm = TRUE))
+  function(mat) log2(matrixStats::rowMads(2^mat, na.rm = TRUE) + 1)
 } else {
-  function(mat) log2(apply(2^mat, 1, mad, na.rm = TRUE))
+  function(mat) log2(apply(2^mat, 1, mad, na.rm = TRUE) + 1)
 }
 
 # ----- CLI arguments -----
